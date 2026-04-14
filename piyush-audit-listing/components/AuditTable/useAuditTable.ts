b59e9baae3b1682b8b31c50e42b0bdd5f6e645c3
piyush-c247 <piyush.m@chapter247.com>
2026-04-14 13:11:57 +0530

    fix: removed unnecessary code

--- piyush-audit-listing/components/AuditTable/useAuditTable.ts
+++ piyush-audit-listing/components/AuditTable/useAuditTable.ts
@@ -7,29 +7,24 @@ export const useAuditTable = () => {
   const sections = getAllSections(auditData);
 
   const mainSections = sections.filter(
-    (sec) => sec.section !== AUDIT_TABLE_FOOTER.BONUS_KEY
+    (sec) => sec.section !== AUDIT_TABLE_FOOTER.BONUS_KEY,
   );
 
   const bonusSection = sections.find(
-    (sec) => sec.section === AUDIT_TABLE_FOOTER.BONUS_KEY
+    (sec) => sec.section === AUDIT_TABLE_FOOTER.BONUS_KEY,
   );
 
-  const totalScore = formatNumber(
-    auditData.sections_total.all_score_total
-  );
+  const totalScore = formatNumber(auditData.sections_total.all_score_total);
 
   const totalWeight = formatPercent(
-    auditData.sections_total.adjustment_weight_section_total
+    auditData.sections_total.adjustment_weight_section_total,
   );
 
   const totalAdjusted = formatNumber(
-    (auditData.sections_total.adjustment_weight_section_total / 100) *
-      auditData.sections_total.all_score_total
+    auditData.sections_total.adjustment_score_section_total,
   );
 
-  const overallSafetyScore = formatNumber(
-    auditData.overall_safety_score_total
-  );
+  const overallSafetyScore = formatNumber(auditData.overall_safety_score_total);
 
   return {
     mainSections,
